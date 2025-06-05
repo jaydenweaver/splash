d81382b4ca73502602692ba2cc2b457dde6273c8
jaydenweaver <jayden_weaver@hotmail.com>
2025-06-05 18:11:55 +1000

    fix project structure

--- src/include/splash.h
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-#include <cmath>
-#include <chrono>
-#include <thread>
-#include <signal.h>
-#include <ncurses.h>
-#include <unistd.h>
--- src/splash.cpp
+++ src/splash.cpp
@@ -1,37 +1,15 @@
-#include <splash.h>
+#include <phys.h>
+#include <renderer.h>
+#include <unistd.h>
 
 bool resized = false;
 void handle_resize(int sig) {resized = true;}
 
 int main() {
-    signal(SIGWINCH, handle_resize);
-    initscr();
-    noecho();
-    curs_set(0);
 
-    int height, width;
-    getmaxyx(stdscr, height, width);
-    int draw = 0;
-
-    while (true) {
-        if (draw >= width) draw = 0;
-
-        if (resized) {
-            resized = false;
-            endwin();
-            refresh();
-            getmaxyx(stdscr, height, width);
-        }
-
-        clear();  
-
-
-        mvaddch(20, draw, '~');
-        draw += 1;
-        refresh();         // redraw screen
+    while (true) {       // redraw screen
         usleep(40000);     // sleep 40ms
     }
-
-    endwin();              
+            
     return 0;
 }
