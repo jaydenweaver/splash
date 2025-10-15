ea56c7f86472e42acb2ea795dc81744114b9af04
Jayden <jayden_weaver@hotmail.com>
2025-10-15 20:34:33 +1000

    performance benchmarking

--- src/splash.cpp
+++ src/splash.cpp
@@ -2,11 +2,18 @@
 #include <renderer.h>
 #include <unistd.h>
 #include <vector>
+#include <chrono>
+#include <iostream>
 
 const int X_RES = 500;
 const int Y_RES = 200;
 
 int main() {
+    // performance benchmarking
+    using clock = std::chrono::high_resolution_clock;
+    uint64_t total_frames = 0;
+    auto start_time = clock::now();
+
     // 'high' resolution 2d grid for simulation - renderer downscales to fit within CLI
     std::vector<int> grid(X_RES * Y_RES, 0);
 
@@ -17,6 +24,8 @@ int main() {
     while (true) {
         int input = getch();
         if (input == 'q') break;
+
+        total_frames++;
 
         if (input == KEY_MOUSE && getmouse(&event) == OK) {
             int terminal_width, terminal_height;
@@ -30,7 +39,23 @@ int main() {
         render(grid, X_RES, Y_RES);
         napms(8);
     }
-    
     close_renderer();
+
+    auto end_time = clock::now();
+    
+    std::chrono::duration<double, std::milli> time_taken_ms = end_time - start_time;
+
+    // average frame time in ms
+    double avg_frame_time_ms = time_taken_ms.count() / total_frames;
+
+    // total time in seconds
+    double time_taken_s = time_taken_ms.count() / 1000.0;
+
+    // average FPS
+    double avg_fps = total_frames / time_taken_s;
+
+    std::cout << "average frame time: " << avg_frame_time_ms << " ms" << std::endl;
+    std::cout << "average fps: " << avg_fps << std::endl;
+
     return 0;
 }
